0a35d1a148741ff3d682b2eb190f620ec14136fc
koki higami <koki5291@gmail.com>
2021-08-10 06:56:39 +0900

    追加：コントローラー

---
+++ src/controller/Controller.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import { User } from '../../User';
+import { MemberRepository } from '../infra/repository/MemberRepository';
+import { MemberRepositoryImpl } from '../infra/repository/MemberRepositoryImpl';
+import { MemberRepositoryImplStub } from '../infra/repository/MemberRepositoryImplStub';
+
+import { MemberService } from '../useCase/MemberService';
+const app: express.Express = express()
+app.get("/", (req: express.Request, res: express.Response) => {
+    // const repo: MemberRepository = new MemberRepositoryImplStub();
+    const repo: MemberRepository = new MemberRepositoryImpl();
+
+    const service: MemberService = new MemberService(repo);
+    const member = service.getMembers(1);
+
+    member.then((m) => {
+        res.json(m)
+    })
+})
+app.listen(3000, () => {
+    console.log('ポート3000番で起動しました。')
+})
